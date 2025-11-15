37656bdd4f6683899ca5560212dd7c8c553c11d3
Kai <Kaiboucher2007@gmail.com>
2025-11-15 22:29:06 +0000

    Update views.py

--- tickets/views.py
+++ tickets/views.py
@@ -109,7 +109,7 @@ def rebuild_ticket_channel(request, ticket_id):
                 "file": (msg.files.name, open(msg.files.path, "rb"))
             }
 
-            text += f"\n\n{msg.files.url}"
+            text += f"\n\nhttps://www.mybustimes.cc{msg.files.url}"
 
         try:
             requests.post(
