f4d04b74b829b1d17d4776d6dd94cfa10b61636f
Kai <Kaiboucher2007@gmail.com>
2025-11-21 11:44:31 +0000

    Update views.py

--- forum/views.py
+++ forum/views.py
@@ -193,7 +193,7 @@ def thread_details_api(request, thread_id):
         else:
             user_badges = []
 
-        if user and user.ad_free_until and user.ad_free_until > timezone.now() and not user.is_staff:
+        if user and user.ad_free_until and user.ad_free_until > timezone.now():
             user_badges.append({
                 "fields": {
                     "badge_name": "Supporter",
