db532310938bf98628ff0f88947f86d3acbbf4c3
Brooklynbull321 <brook1988@outlook.com>
2025-11-24 02:49:11 +0000

    Update views.py

--- account/views.py
+++ account/views.py
@@ -40,8 +40,7 @@ from .forms import CustomUserCreationForm, AccountSettingsForm
 from fleet.models import group, MBTOperator, fleetChange, helper, liverie
 from main.models import CustomUser, UserKeys, badge, StripeSubscription
 from a.models import AffiliateLink, Link
-from group.models import Group
-
+from group.model import group
 import requests
 
 logger = logging.getLogger(__name__)
@@ -203,7 +202,8 @@ def user_profile(request, username):
     # Operators owned by this user
     operators = MBTOperator.objects.filter(owner=profile_user).order_by('operator_slug')
 
-    groups = Group.objects.filter(group_owner=profile_user).order_by('group_name')
+    groups = group.objects.filter(group_owner=profile_user).order_by('group_name')
+
     # Operators the user helps with
     helper_operator_links = helper.objects.filter(helper=profile_user).order_by('operator__operator_name')
     helper_operators_list = MBTOperator.objects.filter(id__in=helper_operator_links.values('operator')).order_by('operator_name')
