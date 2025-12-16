59488c5147911a0297096d373d7f0be1fd3abb8d
Kai <Kaiboucher2007@gmail.com>
2025-12-16 14:37:45 +0000

    Updates Discord message sending
    Updates the way messages are sent to Discord, removing the explicit passing of data as JSON. This change aligns with expected usage and may improve compatibility with the Discord API.

--- fleet/views.py
+++ fleet/views.py
@@ -1727,10 +1727,8 @@ def send_to_discord_for_sale_embed(channel_id, title, message, colour=0x00BFFF, 
     if content:
         response = requests.post(
             f"{settings.DISCORD_BOT_API_URL}/send-message-clean",
-            json={
-                'channel_id': channel_id,
-                'content': content
-            }
+            data=data,
+            files=None
         )
     response.raise_for_status()
 
