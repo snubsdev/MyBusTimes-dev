9d34e36a2a6e8c7f8b40e2ba77a49d2df1bc3325
Brooklynbull321 <brook1988@outlook.com>
2026-04-14 18:30:41 +0100

    Fix syntax error in filter query for existing route

--- fleet/management/commands/import_bt.py
+++ fleet/management/commands/import_bt.py
@@ -151,7 +151,7 @@ class Command(BaseCommand):
 			# Check if route already exists for this operator with same route number
 			existing_route = route.objects.filter(
 			    route_operators=mbt_operator,
-				route_num=line_name
+				route_num=line_name,
 			    inbound_destination=inbound_dest,
 			    outbound_destination=outbound_dest,
 			).first()
